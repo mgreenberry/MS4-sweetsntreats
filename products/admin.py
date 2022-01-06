8c26779b7a7963ba4fcb78614b536d769df2913a
mgreenberry <michaelgreenberry@gmail.com>
2022-01-06 15:44:10 +0000

    customised site admin

---
+++ products/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Product, Category
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
