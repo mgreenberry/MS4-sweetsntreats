f7f6b4747bedba5cdec624108a3f722d98f1bff3
mgreenberry <michaelgreenberry@gmail.com>
2022-01-13 16:20:21 +0000

    added add to basket functionality

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket')
+    path('add/<item.id>/', views.add_to_basket, name='add_to_basket')
 ]
--- basket/views.py
+++ basket/views.py
@@ -7,3 +7,22 @@ def view_basket(request):
     A view to return the index page
     """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """
+    Add a quantity of a product to the shopping basket
+    """
+
+    quantity = request.POST.get('quantity')
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+    
+    request.session['basket'] = basket
+    return redirect_url(redirect_url)
+
