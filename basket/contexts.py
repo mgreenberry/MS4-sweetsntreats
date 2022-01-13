b7e0370397d0bc857bc0ad6bd49aaf89846f614e
mgreenberry <michaelgreenberry@gmail.com>
2022-01-13 15:36:58 +0000

    added basket app, settings, url, and delivery logic

---
+++ basket/contexts.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+from django.conf import settings
+
+def basket_contents(request):
+
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
--- sweetsntreats/settings.py
+++ sweetsntreats/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -149,3 +150,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+
