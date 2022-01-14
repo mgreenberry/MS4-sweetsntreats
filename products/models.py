da6970988787d3ac8c11c61c0551b85bf96ebebf
mgreenberry <michaelgreenberry@gmail.com>
2022-01-14 16:42:26 +0000

    added quantity input +/- buttons

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
