41a45ec42349507673fe686e1bf14ae391c2e4d6
mgreenberry <michaelgreenberry@gmail.com>
2022-02-05 18:01:53 +0000

    updated urls with new basket url

--- basket/urls.py
+++ basket/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>/', views.add_to_basket, name='add_to_basket')
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
