ae61995f792cbcbc4c4c88b3c56a6b6a24424114
mgreenberry <michaelgreenberry@gmail.com>
2022-02-07 17:30:56 +0000

    bringing version 2 upto date. All working

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sweetsntreats.wsgi:application
---
+++ custom_storages.py
@@ -0,0 +1,18 @@
+
+   
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """
+    Static storage for AWS
+    """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """
+    Media storage for AWS
+    """
+    location = settings.MEDIAFILES_LOCATION
