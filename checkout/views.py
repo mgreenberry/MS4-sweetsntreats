83022319193d0967cfc9da27651b75d91a47d665
mgreenberry <michaelgreenberry@gmail.com>
2022-02-08 13:49:14 +0000

    added stripe card element and started css

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-
+        'stripe_public_key': 'pk_test_51KQWFHAfu6MnYyGQ1Z3nIm2QfSs8cwAYjb5aprKVaw1k3sPZDgPM8oCGjCYk4cwO5ajwtBa8bjc4XqaejjG3o5bR00gqFYRWuK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
