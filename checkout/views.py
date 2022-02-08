d8694f4d598826cc5ba7babbc40105d1f799f833
mgreenberry <michaelgreenberry@gmail.com>
2022-02-08 14:28:59 +0000

    started stripe process and added css/js and added to views

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
 
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STIPE_SECRET_KEY
+
     basket = request.session.get('basket',{})
     if not basket:
         message.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        message.warning(request, "Stripe Public Key is missing. \
+            Did you foget to set it in your environment")
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KQWFHAfu6MnYyGQ1Z3nIm2QfSs8cwAYjb5aprKVaw1k3sPZDgPM8oCGjCYk4cwO5ajwtBa8bjc4XqaejjG3o5bR00gqFYRWuK',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
