2880b9053d8f10b6d72280c3c934b614515ae62c
mgreenberry <michaelgreenberry@gmail.com>
2022-02-12 15:00:40 +0000

    added data caching as per Boutique Ado

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,6 +19,7 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
