52f683a8cb3cb646921900353fc681f582af581c
mgreenberry <michaelgreenberry@gmail.com>
2022-02-12 15:38:31 +0000

    completed stripe and checkout function with webhooks

--- checkout/views.py
+++ checkout/views.py
@@ -49,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
             for item_id, item_data in basket.items():
                 try:
                     product = Product.objects.get(id=item_id)
