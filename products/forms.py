ea789dc61ebad1a28194da44755ab1504f5483be
mgreenberry <michaelgreenberry@gmail.com>
2022-02-13 14:51:37 +0000

    created add_product html

--- products/forms.py
+++ products/forms.py
@@ -13,6 +13,6 @@ class ProductForm(forms.ModelForm):
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fiends['category'].choices = friendly_names
+        self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -70,3 +70,16 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """
+    Add a product to the store
+    """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
