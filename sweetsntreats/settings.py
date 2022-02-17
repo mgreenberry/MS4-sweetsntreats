74e017141d72f08dbcec97f55d88e83076b712b6
mgreenberry <michaelgreenberry@gmail.com>
2022-02-17 14:51:45 +0000

    adding heroku as allowed host and deployment started

--- sweetsntreats/settings.py
+++ sweetsntreats/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['sweetsntreats.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,18 @@ WSGI_APPLICATION = 'sweetsntreats.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' is os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
