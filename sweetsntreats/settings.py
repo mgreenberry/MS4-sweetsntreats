831f8a411ff56ed414430398c94155fb39d498b6
mgreenberry <michaelgreenberry@gmail.com>
2022-02-17 16:01:23 +0000

    corrected error in settings.py

--- sweetsntreats/settings.py
+++ sweetsntreats/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'sweetsntreats.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-if 'DATABASE_URL' is os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
