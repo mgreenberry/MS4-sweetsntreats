686da1e17a046d57d6e589dfb2b905cce244409b
mgreenberry <michaelgreenberry@gmail.com>
2022-04-18 14:32:43 +0000

    trying to connect to heroku

--- sweetsntreats/settings.py
+++ sweetsntreats/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['sweetsntreatsv2.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sweetsntreats.herokuapp.com', 'localhost']
 
 
 # Application definition
