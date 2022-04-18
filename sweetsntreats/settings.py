36da34462dfc5face661bd2ef28d196e1ca78ee7
mgreenberry <michaelgreenberry@gmail.com>
2022-04-18 15:33:54 +0000

    trying to fix css issues with final deployment

--- sweetsntreats/settings.py
+++ sweetsntreats/settings.py
@@ -188,9 +188,9 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    # Bucket config
-    AWS_STORAGE_BUCKET_NAME = 'sweetsntreats'
-    AWS_S3_REGION_NAME = 'eu-west-2'
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
